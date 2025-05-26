5a98e020935fc8c62229adabe5b08a096765300c
aayushiJoshi <aayushijoshi9910@gmail.com>
2025-05-27 04:25:47 +0530

    icons as react components

---
+++ src/components/icons/like.tsx
@@ -0,0 +1,21 @@
+import { IoIosHeartEmpty } from "react-icons/io";
+import { IoIosHeart } from "react-icons/io";
+
+
+function LikeIcon({style, state,}:{
+    style?:string,
+    state?:boolean,
+}){
+
+    return (
+        <>
+            {
+                state===true ? 
+                <IoIosHeart className={`${style} active:scale-95 text-pink-like`}/> :
+                <IoIosHeartEmpty className={`${style} text-gray-icon_light active:scale-95`}/>
+            }
+        </>
+    )
+}
+
+export default LikeIcon
