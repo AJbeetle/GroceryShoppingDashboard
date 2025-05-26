7209e8d8d99f381c98bec1b42d64c3a46fc75040
aayushiJoshi <aayushijoshi9910@gmail.com>
2025-05-27 04:25:05 +0530

    loading loaclStorage variables on app mount

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
-import { useState } from 'react'
+import { useState , useEffect} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import DesignSheet from './layouts/devDesignSheet'
 
-import {Like, Cart, Offers} from "./store/loaclStorage"
+import {Like, Cart, Offers, Inventory} from "./store/loaclStorage"
+import axios from 'axios'
 
-localStorage.setItem("Offers",JSON.stringify(Offers));
-localStorage.setItem("Like",JSON.stringify(Like));
-localStorage.setItem("Cart",JSON.stringify(Cart));
+const BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  async function callDB(){
+    const res = await axios.get(`${BASE_URL}/?category=all`);
+    const data = await res.data;
+    return data;
+  }
+  useEffect(function(){
+    localStorage.setItem("Offers",JSON.stringify(Offers));
+    localStorage.setItem("Like",JSON.stringify(Like));
+    localStorage.setItem("Cart",JSON.stringify(Cart));
+    callDB().then(res=>{
+      // const inventoryObj:Record<string, number> = {};
+      for(let i=0; i<res.length;i++){
+        Inventory[`${res[i].name}`] = res[i].available
+      }
+      // console.log(Inventory)
+      localStorage.setItem("Inventory",JSON.stringify(Inventory))
+    });
+  },[])
 
   return (
     <div>
