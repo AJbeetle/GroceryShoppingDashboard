d2fd91c8da11c2f40fb67ca7f0d4cfcdbbd92812
aayushiJoshi <aayushijoshi9910@gmail.com>
2025-05-27 14:39:43 +0530

    tracking with localStorage variables

--- src/components/ui/topBar.tsx
+++ src/components/ui/topBar.tsx
@@ -14,9 +14,21 @@ function TopBar(){
             {/* <SliderIcon className="bg-red-400"></SliderIcon> */}
             <SearchBar size = {"sm"} placeholder="Search" Icon={SliderIcon}></SearchBar>
             <div className="flex w-[15%] justify-around items-center gap-4 ">
-                <LikeIcon state={true} style={`text-[44px] hover:cursor-pointer active:scale-95`}></LikeIcon>
-                <UserIcon style={`hover:cursor-pointer active:scale-95`}></UserIcon>
-                <CartIcon fill={true} style={`w-10 active:scale-95 hover:cursor-pointer`}/>
+                <div className="flex relative">
+                    <LikeIcon state={true} style={`text-[46px] hover:cursor-pointer active:scale-95`}></LikeIcon>
+                    <div className="flex justify-center items-center text-xs absolute -right-4 -top-3 bg-pink-notify text-white-default w-5 h-5 rounded-full">
+                        {JSON.parse(localStorage.getItem("Like") as string )["count"]}
+                    </div>
+                </div>
+                <div>
+                    <UserIcon style={`hover:cursor-pointer active:scale-95`}></UserIcon>
+                </div>
+                <div className="flex relative">
+                    <CartIcon fill={true} style={`w-10 active:scale-95 hover:cursor-pointer`}/>
+                    <div className="flex justify-center items-center text-xs absolute -right-4 -top-3 bg-blue-cart text-white-default w-5 h-5 rounded-full">
+                        {JSON.parse(localStorage.getItem("Cart") as string )["count"]   }
+                    </div>
+                </div>
             </div>
         </div>    
     )
