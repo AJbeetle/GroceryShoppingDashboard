a767e69f462828ca6a598b667d2bc25155c9b3aa
aayushiJoshi <aayushijoshi9910@gmail.com>
2025-05-28 20:19:05 +0530

    design structure for cartCard

--- src/components/ui/cartCard.tsx
+++ src/components/ui/cartCard.tsx
@@ -113,38 +113,64 @@ function CartItemCard({cartItem, setReRender}: {
                     // console.log(pr);
                     // setItemAvailability(invObj[el]);    //it trigerrs inifinite renders 
                     return (
-                        <div className="flex gap-10 text-white-default m-1 bg-pink-300 p-2 w-[80%]" key={el}>
-                            <p className="bg-red-900 p-1">
-                                {el} - {element.name} -  {`P${element.id}`} - {element.price} - {element.img ? `${element.img}` : null}
-                            </p>
-                            <div className="bg-pink-700 p-1">
-                                Quant : 
-                                {
-                                    cartItem.items[el]
-                                }
+                        <div className="flex m-1 bg-pink-400 shadow-lg p-6 rounded-3xl w-[70%] justify-between" key={el}>
+                            {/* Image and Name */}
+                            <div className="flex w-[40%] gap-6 border border-solid border-black-text">
+                                <div>
+                                    <img src={`${element.img}`} className="w-[80px] h-[80px] rounded-lg"></img>
+                                </div>
+                                <div className="flex flex-col gap-4">
+                                    <p className="font-bold text-xl">{element.name}</p>
+                                    <p>Product Code : {`P${element.id}`}</p>
+                                </div>
                             </div>
-                            <div className="bg-blue-600">
-                                
-                                {
-                                    invObj[el] <= 10 ? (invObj[el] == 0 ? `No More after this order` : `Left in Stock : ${invObj[el]}` ): null
-                                    // itemAvailability <= 10 ? (itemAvailability == 0 ? `No More after this order` : `Left in Stock : ${itemAvailability}` ): null  //triggers infinite renders
-                                }
-                            </div>   
-                            <div className="bg-green-600 p-1">
-                                Price : {(parseFloat((element.price).split("£")[1])*cartItem.items[el]).toFixed(2)}
-                                {/* Price : {pr} */}
-                            </div>    
-                            <div className="flex gap-2 ">
-                                <button className="active:scale-90 disabled:scale-100 disabled:cursor-not-allowed" onClick={()=>addToCART(element)} disabled={invObj[el]===0?true:false}> 
-                                    <AddIcon/>
-                                </button>
-                                <button className="active:scale-90 disabled:scale-100 disabled:cursor-not-allowed" onClick={()=>minusFromCART(element)} disabled={cartObj.items[el]===0?true:false}>
-                                    <MinusIcon/>
-                                </button>
-                                <button className="active:scale-90 disabled:scale-100 disabled:cursor-not-allowed" onClick={()=>removeFromCART(element)} disabled={false}>
-                                    <RemoveIcon/>
-                                </button>
-                            </div>   
+
+                            {/* Other Functionalities */}
+                            <div className="flex justify-between w-[60%] border border-solid border-yellow-300 items-start">
+                                <div className="flex flex-col w-full gap-2">
+                                    <div className="flex justify-center items-center w-full border border-sollid border-black-text">
+                                        <div className="flex gap-4 w-[70%] border border-solid border-black-text justify-center">
+                                            <button className="active:scale-90 disabled:scale-100 disabled:cursor-not-allowed" onClick={()=>minusFromCART(element)} disabled={cartObj.items[el]===0?true:false}>
+                                                <MinusIcon/>
+                                            </button>
+                                            <p className="text-xl">
+                                                {
+                                                    cartItem.items[el]
+                                                }
+                                            </p>
+                                            <button className="active:scale-90 disabled:scale-100 disabled:cursor-not-allowed" onClick={()=>addToCART(element)} disabled={invObj[el]===0?true:false}> 
+                                                <AddIcon/>
+                                            </button>
+                                        </div>
+
+                                        <div className="flex justify-end items-center border border-solid border-red-600 gap-14 w-[30%]  ">
+                                            <div>
+                                                £{(parseFloat((element.price).split("£")[1])*cartItem.items[el]).toFixed(2)}
+                                            </div>
+
+                                            <div className=" border border-solid border-blue-500 justify-center items-center flex">
+                                                <button className="active:scale-90 disabled:scale-100 disabled:cursor-not-allowed" onClick={()=>removeFromCART(element)} disabled={false}>
+                                                    <RemoveIcon/>
+                                                </button>
+                                            </div>   
+                                        </div>   
+                                    </div>   
+
+                                    <div className="w-[70%] border border-solid border-black-text flex justify-center">
+                                        {
+                                            invObj[el] > 10 ? null :
+                                            <div className="flex justify-center items-center w-fit bg-orange-base opacity-50 text-white-default text-xs p-2 rounded-lg">
+                                                {
+                                                    invObj[el] <= 10 ? (invObj[el] == 0 ? `no more allowed` : `Only ${invObj[el]} Left` ): null
+                                                }
+                                            </div>   
+                                        }
+                                    </div>    
+
+                                    
+                                </div>
+
+                            </div>
                         </div>
                     )
                 })
