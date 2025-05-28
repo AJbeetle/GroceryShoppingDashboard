5af51e6eac9d92f7f0eb3b3f65ffdf8ed67c4464
aayushiJoshi <aayushijoshi9910@gmail.com>
2025-05-28 18:30:24 +0530

    designSheet updated with latest UI

--- src/layouts/devDesignSheet.tsx
+++ src/layouts/devDesignSheet.tsx
@@ -7,6 +7,7 @@ import TopBar from "../components/ui/topBar";
 import { PiSlidersHorizontalFill } from "react-icons/pi";
 import {useRef, useState, useEffect} from "react"
 import type { LikeObjType, InvObjType, CartObjType } from "../store/loaclStorage";
+import CartItemCard from "../components/ui/cartCard";
 
 function DesignSheet(){
     const [item, setItem ] = useState(false); //this is just a state atom to be triggered in order to re render this design page : if something is added to cart, or liked from user in search panel
@@ -17,6 +18,7 @@ function DesignSheet(){
     const bakery:apiItems[] = useRecoilValue(bakerySelector);
     const searchs:apiItems[] = useRecoilValue(searchItemsSelector);
     const cartItemsCount = useRecoilValue(cartCountAtom);
+    const [cartReRender, setCartReRender] = useState(false);
 
     // const [view, setView] : [cardItems[] | apiItems[], React.Dispatch<React.SetStateAction<undefined>> | any] = useState(items);
     const [view, setView] = useState<cardItems[] | apiItems[]>(items);
@@ -50,79 +52,82 @@ function DesignSheet(){
 
                 // Non-scrollable design
                 <div className="flex flex-wrap flex-col w-[100%] justify-center p-2 items-start"> 
-                <div className="flex overflow-hidden justify-start items-center flex-wrap">
-                    {
-                        searchs.map((el:apiItems | cardItems,i:number) => {
-                            const cartAvail = (CartObj.items[el.name]==0 || CartObj.items[el.name]==undefined) ? false : true;
-                            const likeState = (LikeObject.items[el.name]===true) ? true : false;
-                            const invCount = InvObj[el.name]
+                    <div className="flex overflow-hidden justify-start items-center flex-wrap">
+                        {
+                            searchs.map((el:apiItems | cardItems,i:number) => {
+                                const cartAvail = (CartObj.items[el.name]==0 || CartObj.items[el.name]==undefined) ? false : true;
+                                const likeState = (LikeObject.items[el.name]===true) ? true : false;
+                                const invCount = InvObj[el.name]
 
-                            return (
-                                    //@ts-ignore
-                                    <ItemCard item={el} likeState={likeState} cartState={cartAvail} key={el.id || el.name} userSessionItemAvailable={invCount} setExtraRender={setItem}></ItemCard>
-                                )
+                                return (
+                                        //@ts-ignore
+                                        <ItemCard item={el} likeState={likeState} cartState={cartAvail} key={el.id || el.name} userSessionItemAvailable={invCount} setExtraRender={setItem}></ItemCard>
+                                    )
 
                             
-                        })
-                    }
+                            })  
+                        }
+                    </div>
+                </div> :
+                <div>
+                    {/* ELEMENT 3 : CATEORIES BUTTONS --------------------------------------------------------------------------------------------- */}
+                    <div className="m-5 p-2 flex w-full justify-start gap-8">
+                        <button className="bg-white-default w-[140px] rounded-full border-2 border-solid border-gray-stroke px-6 py-2 shadow-lg active:scale-95 " onClick={()=>changeView(items)}>
+                            All Items
+                        </button>
+                        <button className="bg-white-default w-[140px] rounded-full border-2 border-solid border-gray-stroke px-6 py-2 shadow-lg active:scale-95" onClick={()=>changeView(drinks)}>
+                            Drinks
+                        </button>
+                        <button className="bg-white-default w-[140px] rounded-full border-2 border-solid border-gray-stroke px-6 py-2 shadow-lg active:scale-95 " onClick={()=>changeView(fruits)}>
+                            Fruits
+                        </button>
+                        <button className="bg-white-default w-[140px] rounded-full border-2 border-solid border-gray-stroke px-6 py-2 shadow-lg active:scale-95 " onClick={()=>changeView(bakery)}>
+                            Bakery
+                        </button>
+                    </div>
+
+                    {/* ELEMENT 4 : ITEMS LIST IN FORM OF CARDS --------------------------------------------------------------------------------------------- */}
+                    <div className="flex flex-wrap">
+                        {
+                            view.map((el:apiItems | cardItems,i:number) => {
+                                //  console.log(el);
+                                const cartAvail = (CartObj.items[el.name]==0 || CartObj.items[el.name]==undefined) ? false : true;
+                                const likeState = (LikeObject.items[el.name]===true) ? true : false;
+                                const invCount = InvObj[el.name]
+
+                                // console.log(`${CartObj.items[el.name]} : ${cartAvail}`);
+                                // console.log(`${LikeObject.items[el.name]} : ${likeState}`);
+                                // console.log(`${InvObj[el.name]} : ${invCount}`);
+
+                                return (
+                                    // bro I just spent last 5-6 hours finding what the error is in my UI, that my cards are not displaying props properly : and it was this key prop which was skdnsakdj the UI
+                                    // DAAMMMNNNN NOW I UNDERSTOOD THE REAL RELEVANCE OF *********KEY*********
+                                        //@ts-ignore
+                                        <ItemCard item={el} likeState={likeState} cartState={cartAvail} key={el.id || el.name} userSessionItemAvailable={invCount}></ItemCard>
+                                )
+
+                                // if(LikeObject.current.items[`${el.name}`]){
+                                //     console.log(`${el.name} : ${CartObj.current.items[el.name]} : `);
+                                //     return (
+                                //         <ItemCard item={el} likeState={true} cartState={cartAvail} key={i} userSessionItemAvailable={InvObj.current[`${el.name}`]}></ItemCard>
+                                //     )
+                                // }
+                                // else{
+                                //     console.log(`${el.name} : ${CartObj.current.items[el.name]}`);
+                                //     return (
+                                //         <ItemCard item={el} likeState={false} cartState={cartAvail} key={i} userSessionItemAvailable={InvObj.current[`${el.name}`]}></ItemCard>
+                                //     )
+                                // }
+                            })
+                        }
+                    </div>
                 </div>
-            </div> :
-            <div>
-                {/* ELEMENT 3 : CATEORIES BUTTONS --------------------------------------------------------------------------------------------- */}
-                <div className="m-5 p-2 flex w-full justify-start gap-8">
-                    <button className="bg-white-default w-[140px] rounded-full border-2 border-solid border-gray-stroke px-6 py-2 shadow-lg active:scale-95 " onClick={()=>changeView(items)}>
-                        All Items
-                    </button>
-                    <button className="bg-white-default w-[140px] rounded-full border-2 border-solid border-gray-stroke px-6 py-2 shadow-lg active:scale-95" onClick={()=>changeView(drinks)}>
-                        Drinks
-                    </button>
-                    <button className="bg-white-default w-[140px] rounded-full border-2 border-solid border-gray-stroke px-6 py-2 shadow-lg active:scale-95 " onClick={()=>changeView(fruits)}>
-                        Fruits
-                    </button>
-                    <button className="bg-white-default w-[140px] rounded-full border-2 border-solid border-gray-stroke px-6 py-2 shadow-lg active:scale-95 " onClick={()=>changeView(bakery)}>
-                        Bakery
-                    </button>
-                </div>
-
-                {/* ELEMENT 4 : ITEMS LIST IN FORM OF CARDS --------------------------------------------------------------------------------------------- */}
-                <div className="flex flex-wrap">
-                    {
-                        view.map((el:apiItems | cardItems,i:number) => {
-                            //  console.log(el);
-                            const cartAvail = (CartObj.items[el.name]==0 || CartObj.items[el.name]==undefined) ? false : true;
-                            const likeState = (LikeObject.items[el.name]===true) ? true : false;
-                            const invCount = InvObj[el.name]
-
-                            // console.log(`${CartObj.items[el.name]} : ${cartAvail}`);
-                            // console.log(`${LikeObject.items[el.name]} : ${likeState}`);
-                            // console.log(`${InvObj[el.name]} : ${invCount}`);
-
-                            return (
-                                // bro I just spent last 5-6 hours finding what the error is in my UI, that my cards are not displaying props properly : and it was this key prop which was skdnsakdj the UI
-                                // DAAMMMNNNN NOW I UNDERSTOOD THE REAL RELEVANCE OF *********KEY*********
-                                    //@ts-ignore
-                                    <ItemCard item={el} likeState={likeState} cartState={cartAvail} key={el.id || el.name} userSessionItemAvailable={invCount}></ItemCard>
-                            )
-
-                            // if(LikeObject.current.items[`${el.name}`]){
-                            //     console.log(`${el.name} : ${CartObj.current.items[el.name]} : `);
-                            //     return (
-                            //         <ItemCard item={el} likeState={true} cartState={cartAvail} key={i} userSessionItemAvailable={InvObj.current[`${el.name}`]}></ItemCard>
-                            //     )
-                            // }
-                            // else{
-                            //     console.log(`${el.name} : ${CartObj.current.items[el.name]}`);
-                            //     return (
-                            //         <ItemCard item={el} likeState={false} cartState={cartAvail} key={i} userSessionItemAvailable={InvObj.current[`${el.name}`]}></ItemCard>
-                            //     )
-                            // }
-                        })
-                    }
-                </div>
-            </div>
             }
             
-
+            {/* ELEMENT 5 : ITEMS LIST IN FORM OF CARDS --------------------------------------------------------------------------------------------- */}
+            <div className="border border-solid border-black-text mt-10 px-2 py-4">
+                <CartItemCard cartItem={CartObj} setReRender={setCartReRender}/>
+            </div>
             
             
         </div>
