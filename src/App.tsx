9d03a9fac3a1ad8dce82e5d834bbf6fe1c6802b1
aayushiJoshi <aayushijoshi9910@gmail.com>
2025-05-28 18:31:16 +0530

    localStorage dummy load on firstRender and then inside useEffect [wrapped in try-catch]

--- src/App.tsx
+++ src/App.tsx
@@ -10,31 +10,48 @@ import axios from 'axios'
 const BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL;
 
 function App() {
-  const [count, setCount] = useState(0);
 
   async function callDB(){
     const res = await axios.get(`${BASE_URL}/?category=all`);
     const data = await res.data;
     return data;
   }
+
+  localStorage.setItem("Offers",JSON.stringify(Offers));
+  localStorage.setItem("Like",JSON.stringify(Like));
+  localStorage.setItem("Cart",JSON.stringify(Cart));
+  localStorage.setItem("Inventory",JSON.stringify(Inventory));
+
   useEffect(function(){
-    localStorage.setItem("Offers",JSON.stringify(Offers));
-    localStorage.setItem("Like",JSON.stringify(Like));
-    localStorage.setItem("Cart",JSON.stringify(Cart));
-    callDB().then(res=>{
-      // const inventoryObj:Record<string, number> = {};
-      for(let i=0; i<res.length;i++){
-        const cartItems = JSON.parse(localStorage.getItem("Cart") as string)["items"];
-        if(cartItems[res[i].name]){
-          Inventory[`${res[i].name}`] = res[i].available - cartItems[res[i].name]
+    console.log("Setting localStorage");
+    try{
+      localStorage.setItem("Offers",JSON.stringify(Offers));
+      localStorage.setItem("Like",JSON.stringify(Like));
+      localStorage.setItem("Cart",JSON.stringify(Cart));
+      localStorage.setItem("Inventory",JSON.stringify(Inventory));
+      callDB().then(res=>{
+        // const inventoryObj:Record<string, number> = {};
+        for(let i=0; i<res.length;i++){
+          const cartItems = JSON.parse(localStorage.getItem("Cart") as string)["items"];
+          if(cartItems[res[i].name]){
+            Inventory[`${res[i].name}`] = res[i].available - cartItems[res[i].name]
+          }
+          else{
+            Inventory[`${res[i].name}`] = res[i].available
+          }
         }
-        else{
-          Inventory[`${res[i].name}`] = res[i].available
-        }
-      }
-      // console.log(Inventory)
-      localStorage.setItem("Inventory",JSON.stringify(Inventory))
-    });
+        // console.log(Inventory)
+        localStorage.setItem("Inventory",JSON.stringify(Inventory))
+      });
+
+    }
+    catch(E){
+      console.log(E);
+    }
+
+    return function(){
+      localStorage.clear();
+    }
   },[])
 
   return (
